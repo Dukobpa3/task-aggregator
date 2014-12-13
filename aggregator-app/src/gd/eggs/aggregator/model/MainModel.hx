4409e6a7e949c6586f1d286fef5e600434391d49
Dukobpa3 <a.burdun@gmail.com>
2014-12-13 20:30:42 +0200

    save state

--- aggregator-app/src/gd/eggs/aggregator/model/MainModel.hx
+++ aggregator-app/src/gd/eggs/aggregator/model/MainModel.hx
@@ -2,10 +2,12 @@ package gd.eggs.aggregator.model;
 import flash.events.Event;
 import flash.events.EventDispatcher;
 import flash.events.IOErrorEvent;
+import flash.Lib;
 import flash.net.URLLoader;
 import flash.net.URLLoaderDataFormat;
 import flash.net.URLRequest;
 import flash.net.URLRequestHeader;
+import flash.net.URLVariables;
 
 /**
  * ...
@@ -21,6 +23,8 @@ class MainModel extends EventDispatcher
 	//=========================================================================
 	inline static var ASANA_API_KEY:String = "2k9Fibx.pdmzr6ZqIiQQsXszJVjiaBeO";
 	inline static var ASANA_API_KEY_ENCODED:String = "Mms5RmlieC5wZG16cjZacUlpUVFzWHN6SlZqaWFCZU86";
+	inline static var ASANA_APP_KEY:String = "10257151362570";
+	inline static var ASANA_SECRET:String = "ab577f9652ab6628df41b8733f8accab";
 	//=========================================================================
 	//	PARAMETERS
 	//=========================================================================
@@ -42,14 +46,30 @@ class MainModel extends EventDispatcher
 	//=========================================================================
 	public function init()
 	{
+		
+		//client_id - required The Client ID uniquely identifies the application making the request.
+		//redirect_uri - required The URI to redirect to on success or error. This must match the Redirect URL specified in the application settings.
+		//response_type - required Must be one of either code (if using the Authorization Code Grant flow) or token (if using the I
+		
 		var _request:URLRequest = new URLRequest();
-		var header:URLRequestHeader = new URLRequestHeader("Authorization", "Basic " + ASANA_API_KEY_ENCODED);
-		_request.requestHeaders = [header];
-		_request.url = "https://app.asana.com/api/1.0/users/me?opt_pretty";
+		var client_id:URLRequestHeader = new URLRequestHeader('client_id', ASANA_APP_KEY);
+		var redirect_uri:URLRequestHeader = new URLRequestHeader('redirect_uri', 'http://task.eggs.gd');
+		var response_type:URLRequestHeader = new URLRequestHeader('response_type', 'code');
+		
+		_request.requestHeaders = [client_id, redirect_uri, response_type];
+		_request.url = "https://www.dropbox.com/1/oauth2/authorize";
+		
+		var vars = new URLVariables();
+		vars.client_id = 'q1u7cqfy3s01j6g';
+		//vars.redirect_uri = 'http://localhost';
+		vars.response_type = 'code';
+		
+		_request.data = vars;
 		
 		_loader.addEventListener(Event.COMPLETE, onLoaderComplete);
 		_loader.addEventListener(IOErrorEvent.IO_ERROR, onError);
-		_loader.load(_request);
+		//_loader.load(_request);
+		Lib.getURL(_request);
 	}
 	//=========================================================================
 	//	PRIVATE
